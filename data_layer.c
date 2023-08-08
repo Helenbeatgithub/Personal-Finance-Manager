fcf4c164c17a410d7d5188f57b59c3a41efb1d26
YunHao12 <128429226+YunHao12@users.noreply.github.com>
2023-08-08 16:56:54 -0700

    Update data_layer.c

--- data_layer.c
+++ data_layer.c
@@ -1,232 +1,157 @@
+#include "data_layer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <mysql/mysql.h>
-#include "data_layer.h"
 
-// Database configuration
-#define DB_HOST "localhost"
-#define DB_USER "username"
-#define DB_PASS "password"
-#define DB_NAME "finance_manager"
+MYSQL *conn;
 
-#define CREATE_SUCCESS 0
-#define CREATE_FAILURE 1
+void initializeDataLayer() {
+    conn = mysql_init(NULL);
 
-// Database connection object
-static MYSQL* connection;
-
-
-void initializeDatabase() {
-    connection = mysql_init(NULL);
-
-    if (connection == NULL) {
+    if (conn == NULL) {
         fprintf(stderr, "mysql_init() failed\n");
         exit(1);
     }
 
-    if (mysql_real_connect(connection, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0) == NULL) {
-        fprintf(stderr, "mysql_real_connect() failed: %s\n", mysql_error(connection));
-        mysql_close(connection);
+    if (mysql_real_connect(conn, "localhost", "username", "password", "database_name", 0, NULL, 0) == NULL) {
+        fprintf(stderr, "mysql_real_connect() failed\n");
+        mysql_close(conn);
         exit(1);
     }
-
-    // Create tables if they don't exist
-    const char* createIncomeTableSQL =
-        "CREATE TABLE IF NOT EXISTS income ("
-        "  id INT AUTO_INCREMENT PRIMARY KEY,"
-        "  year INT NOT NULL,"
-        "  month INT NOT NULL,"
-        "  day INT NOT NULL,"
-        "  category INT NOT NULL,"
-        "  description VARCHAR(255) NOT NULL,"
-        "  amount FLOAT NOT NULL"
-        ");";
-
-    const char* createExpenseTableSQL =
-        "CREATE TABLE IF NOT EXISTS expense ("
-        "  id INT AUTO_INCREMENT PRIMARY KEY,"
-        "  year INT NOT NULL,"
-        "  month INT NOT NULL,"
-        "  day INT NOT NULL,"
-        "  category INT NOT NULL,"
-        "  description VARCHAR(255) NOT NULL,"
-        "  amount FLOAT NOT NULL"
-        ");";
-
-    if (mysql_query(connection, createIncomeTableSQL) != 0) {
-        fprintf(stderr, "Error creating income table: %s\n", mysql_error(connection));
-    }
-
-    if (mysql_query(connection, createExpenseTableSQL) != 0) {
-        fprintf(stderr, "Error creating expense table: %s\n", mysql_error(connection));
-    }
 }
 
-void closeDatabase() {
-    mysql_close(connection);
+void closeDataLayer() {
+    mysql_close(conn);
 }
 
-int createNewPersonalFinance(struct PersonalFinance* pf) {
-    // Implement SQL INSERT query and execute it
+int* addPersonalFinance(struct PersonalFinance* pf) {
+    // Implement SQL INSERT query for personal_finance table
     char query[256];
-    sprintf(query, "INSERT INTO personal_finance (total_income, total_expense, income_count, expense_count) "
-                   "VALUES (%f, %f, %d, %d)", pf->total_income, pf->total_expense, pf->income_count, pf->expense_count);
+    snprintf(query, sizeof(query), "INSERT INTO personal_finance (personalFinanceId, income, expense) VALUES (%d, %f, %f)",
+             pf->personalFinanceId, pf->income, pf->expense);
 
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Insert error: %s\n", mysql_error(conn));
-        return CREATE_FAILURE; 
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "INSERT error: %s\n", mysql_error(conn));
+        return NULL;
     }
 
-    return CREATE_SUCCESS;
+    return NULL;
 }
 
-int addTransactionToPersonalFinance(struct Transaction* t) {
-    // Implement SQL INSERT query and execute it
+int* addTransaction(int personal_finance_id, struct Transaction* t) {
+    // Implement SQL INSERT query for transactions table
     char query[256];
-    sprintf(query, "INSERT INTO transactions (personal_finance_id, year, month, day, type, category, description, amount) "
-                   "VALUES (%d, %d, %d, %d, '%s', '%s', '%s', %f)",
-            t->personal_finance_id, t->year, t->month, t->day, t->type == Income ? "Income" : "Expense",
-            t->category == Groceries ? "Groceries" : t->category == Utilities ? "Utilities" :
-            t->category == Rent ? "Rent" : t->category == Salary ? "Salary" :
-            t->category == Transportation ? "Transportation" : "Dining",
-            t->description, t->amount);
+    snprintf(query, sizeof(query),
+             "INSERT INTO transactions (transactionId, personalFinanceId, year, month, day, type, category, amount, status) VALUES (%d, %d, %d, %d, %d, '%s', '%s', %f, %d)",
+             t->transactionId, personal_finance_id, t->year, t->month, t->day, t->type, t->category, t->amount, t->status);
 
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Insert error: %s\n", mysql_error(conn));
-        return CREATE_FAILURE;
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "INSERT error: %s\n", mysql_error(conn));
+        return NULL;
     }
 
-    return CREATE_SUCCESS;
+    return NULL;
 }
 
-int updatePersonalFinanceData(struct PersonalFinance* pf) {
-    // Implement SQL UPDATE query and execute it
-    char query[256];
-    sprintf(query, "UPDATE personal_finance SET total_income = %f, total_expense = %f, income_count = %d, expense_count = %d "
-                   "WHERE id = %d", pf->total_income, pf->total_expense, pf->income_count, pf->expense_count, pf->id);
+int* deleteTransaction(int transaction_id) {
+    // Implement SQL DELETE query for transactions table
+    char query[128];
+    snprintf(query, sizeof(query), "DELETE FROM transactions WHERE transactionId = %d", transaction_id);
 
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Update error: %s\n", mysql_error(conn));
-        return CREATE_FAILURE;
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "DELETE error: %s\n", mysql_error(conn));
+        return NULL;
     }
-    return CREATE_SUCCESS;
+
+    return NULL;
 }
-、
 
-int updateTransactionData(struct Transaction* t) {
-    // Implement SQL UPDATE query and execute it
+int* updateTransaction(struct Transaction* t) {
+    // Implement SQL UPDATE query for transactions table
     char query[256];
-    sprintf(query, "UPDATE transactions SET year = %d, month = %d, day = %d, type = '%s', category = '%s', "
-                   "description = '%s', amount = %f WHERE id = %d",
-            t->year, t->month, t->day, t->type == Income ? "Income" : "Expense",
-            t->category == Groceries ? "Groceries" : t->category == Utilities ? "Utilities" :
-            t->category == Rent ? "Rent" : t->category == Salary ? "Salary" :
-            t->category == Transportation ? "Transportation" : "Dining",
-            t->description, t->amount, t->id);
+    snprintf(query, sizeof(query),
+             "UPDATE transactions SET year = %d, month = %d, day = %d, type = '%s', category = '%s', amount = %f, status = %d WHERE transactionId = %d",
+             t->year, t->month, t->day, t->type, t->category, t->amount, t->status, t->transactionId);
 
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Update error: %s\n", mysql_error(conn));
-        return CREATE_FAILURE;
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "UPDATE error: %s\n", mysql_error(conn));
+        return NULL;
     }
-    return CREATE_SUCCESS;
+
+    return NULL;
+}
+
+struct PersonalFinance* loadPersonalFinanceData(int personal_finance_id) {
+    // Implement SQL SELECT query for personal_finance table
+    char query[128];
+    snprintf(query, sizeof(query), "SELECT * FROM personal_finance WHERE personalFinanceId = %d", personal_finance_id);
+
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "SELECT error: %s\n", mysql_error(conn));
+        return NULL;
+    }
+
+    MYSQL_RES *result = mysql_store_result(conn);
+    if (!result) {
+        fprintf(stderr, "mysql_store_result() failed\n");
+        return NULL;
+    }
+
+    MYSQL_ROW row = mysql_fetch_row(result);
+    if (!row) {
+        fprintf(stderr, "No data found\n");
+        mysql_free_result(result);
+        return NULL;
+    }
+
+    struct PersonalFinance* pf = (struct PersonalFinance*)malloc(sizeof(struct PersonalFinance));
+    pf->personalFinanceId = atoi(row[0]);
+    pf->income = atof(row[1]);
+    pf->expense = atof(row[2]);
+
+    mysql_free_result(result);
+    return pf;
 }
 
 struct Transaction* loadAllTransactions(int personal_finance_id) {
-    char query[512];
-    sprintf(query, "SELECT * FROM transactions WHERE personal_finance_id = %d", personal_finance_id);
+    // Implement SQL SELECT query for transactions table
+    char query[128];
+    snprintf(query, sizeof(query), "SELECT * FROM transactions WHERE personalFinanceId = %d", personal_finance_id);
 
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Select error: %s\n", mysql_error(conn));
-        return NULL; // Return NULL to indicate an error
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "SELECT error: %s\n", mysql_error(conn));
+        return NULL;
     }
 
-    MYSQL_RES* result = mysql_store_result(conn);
-    if (result == NULL) {
-        fprintf(stderr, "Store result error: %s\n", mysql_error(conn));
-        return NULL; // Return NULL to indicate an error
+    MYSQL_RES *result = mysql_store_result(conn);
+    if (!result) {
+        fprintf(stderr, "mysql_store_result() failed\n");
+        return NULL;
     }
 
     int num_transactions = mysql_num_rows(result);
-    struct Transaction* transactions = (struct Transaction*)malloc(num_transactions * sizeof(struct Transaction));
+    if (num_transactions == 0) {
+        fprintf(stderr, "No transactions found\n");
+        mysql_free_result(result);
+        return NULL;
+    }
 
+    struct Transaction* transactions = (struct Transaction*)malloc(num_transactions * sizeof(struct Transaction));
     int i = 0;
     MYSQL_ROW row;
     while ((row = mysql_fetch_row(result)) != NULL) {
-        struct Transaction* t = &transactions[i];
-        t->id = atoi(row[0]);
-        t->personal_finance_id = atoi(row[1]);
-        t->year = atoi(row[2]);
-        t->month = atoi(row[3]);
-        t->day = atoi(row[4]);
-        t->type = (strcmp(row[5], "Income") == 0) ? Income : Expense;
-        t->category = parseCategory(row[6]);
-        strncpy(t->description, row[7], sizeof(t->description) - 1);
-        t->amount = atof(row[8]);
+        transactions[i].transactionId = atoi(row[0]);
+        transactions[i].year = atoi(row[2]);
+        transactions[i].month = atoi(row[3]);
+        transactions[i].day = atoi(row[4]);
+        strcpy(transactions[i].type, row[5]);
+        strcpy(transactions[i].category, row[6]);
+        transactions[i].amount = atof(row[7]);
+        transactions[i].status = atoi(row[8]);
         i++;
     }
 
     mysql_free_result(result);
     return transactions;
-}
-
-// Load a specific PersonalFinance entry by its ID
-struct PersonalFinance* loadPersonalFinanceData(int personal_finance_id) {
-    char query[256];
-    sprintf(query, "SELECT * FROM personal_finance WHERE id = %d", personal_finance_id);
-
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Select error: %s\n", mysql_error(conn));
-        return NULL; // Return NULL to indicate an error
-    }
-
-    MYSQL_RES* result = mysql_store_result(conn);
-    if (result == NULL) {
-        fprintf(stderr, "Store result error: %s\n", mysql_error(conn));
-        return NULL; // Return NULL to indicate an error
-    }
-
-    MYSQL_ROW row = mysql_fetch_row(result);
-    if (row != NULL) {
-        struct PersonalFinance* pf = (struct PersonalFinance*)malloc(sizeof(struct PersonalFinance));
-        pf->id = atoi(row[0]);
-        pf->total_income = atof(row[1]);
-        pf->total_expense = atof(row[2]);
-        pf->income_count = atoi(row[3]);
-        pf->expense_count = atoi(row[4]);
-
-        mysql_free_result(result);
-        return pf;
-    } else {
-        fprintf(stderr, "Personal finance entry with ID %d not found.\n", personal_finance_id);
-        mysql_free_result(result);
-        return NULL; // Return NULL to indicate an error or not found
-    }
-}
-
-
-
-int deletePersonalFinanceData(struct PersonalFinance* pf) {
-    // Implement SQL DELETE query and execute it
-    char query[256];
-    sprintf(query, "DELETE FROM personal_finance WHERE id = %d", pf->id);
-
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Delete error: %s\n", mysql_error(conn));
-        return CREATE_FAILURE;
-    }
-    return CREATE_SUCCESS;
-}
-
-// Delete a specific transaction entry based on ID
-int deleteTransactionData(struct Transaction* t) {
-    // Implement SQL DELETE query and execute it
-    char query[256];
-    sprintf(query, "DELETE FROM transactions WHERE id = %d", t->id);
-
-    if (mysql_query(conn, query) != 0) {
-        fprintf(stderr, "Delete error: %s\n", mysql_error(conn));
-        return CREATE_FAILURE;
-    }
-    return CREATE_SUCCESS;
 }
