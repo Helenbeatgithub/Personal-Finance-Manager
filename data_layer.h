992b7fa7faecdeaf0e9a0b1ab2fb7e1945307883
YunHao12 <128429226+YunHao12@users.noreply.github.com>
2023-08-08 15:08:54 -0700

    Update data_layer.h

--- data_layer.h
+++ data_layer.h
@@ -6,15 +6,11 @@
 void initializeDataLayer();
 void closeDataLayer();
 
-int* createNewPersonalFinance(struct PersonalFinance* pf);
-int* addTransactionToPersonalFinance(struct Transaction* t);
-
-int* updatePersonalFinanceData(struct PersonalFinance* pf);
-int* updateTransactionData(struct Transaction* t);
-
+int* addPersonalFinance(struct PersonalFinance* pf); 
+int* addTransaction(struct Transaction* t);
+int* deleteTransaction(int transaction_id);
+int* updateTransaction(struct Transaction* t);
 struct PersonalFinance* loadPersonalFinanceData(int personal_finance_id);
 struct Transaction* loadAllTransactions(int personal_finance_id);
 
-int* deletePersonalFinanceData(struct PersonalFinance* pf);
-int* deleteTransactionData(struct Transaction* t);
 #endif 
